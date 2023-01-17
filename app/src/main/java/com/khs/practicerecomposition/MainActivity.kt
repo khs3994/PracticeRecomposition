88bba7eb4664f2f891cd27c08e614d9ccbafbbae
khs3994 <z01039948003@gmail.com>
2023-01-17 20:33:10 +0900

    :sparkles: :: recomposition에 관한 예제

--- app/src/main/java/com/khs/practicerecomposition/MainActivity.kt
+++ app/src/main/java/com/khs/practicerecomposition/MainActivity.kt
@@ -3,41 +3,38 @@ package com.khs.practicerecomposition
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.khs.practicerecomposition.ui.theme.PracticeRecompositionTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PracticeRecompositionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            var num by remember {
+                mutableStateOf(0)
             }
+
+            println("Text - setContent recomposition")//text recomposeScope : start
+            Text(
+                text = "Click me! count: $num",
+                modifier = Modifier.clickable { num++ }
+            ).also { println("Text - Text Recomposition!") } //end
+
+
+            println("Text with Btn - setContent recomposition!")
+            Button(onClick = { num++ }) {//btn recomposeScope : start
+                Text(//text recomposeScope : start
+                    text = "Click me! count: $num",
+                ).also {
+                    println("Text with Btn - Text recomposition!")
+                }//end
+            }.also { println("Text with Btn - Button Recomposition!") }//end
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PracticeRecompositionTheme {
-        Greeting("Android")
     }
 }
